aa69836294de5c32398f4fb1af54359aaed87ca0
Phakphoom Khumpuan <6303013002@feu.edu>
2020-12-05 23:34:19 +0700

    deploy to heroku

---
+++ components/footer/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="footer">
+    <div class="columns container">
+      <div class="column">
+        <p>CopyRight 2020</p>
+      </div>
+    <div class="column has-text-right">
+      <p>Board Game Shop</p>
+    </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VmFooter',
+}
+</script>
+
+<style lang="scss" scoped>
+ .footer {
+   background: #b9e2fc;
+   color: black;
+ }
+</style>
